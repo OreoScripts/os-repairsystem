7791986a3f4caa3b6dfefb305fbea04e31adf36d
NaorShabtai <111662813+NaorShabtai@users.noreply.github.com>
2023-05-01 16:16:36 +0300

    Fix locations
    Now you can add any locations you want

--- cv-repairsystem/repairsystem-sh.lua
+++ cv-repairsystem/repairsystem-sh.lua
@@ -2,16 +2,29 @@ Config = {}
 
 Config.Mechanics = {
     { -- Mechanic 1
-        coords = vec3(-212.0682, -1324.342, 30.468223),
-        showBlip = true,
-        blipData = {
-            sprite = 72,
-            display = 4,
-            scale = 0.65,
-            colour = 66,
-            title = "Bennys",
+        coords = vec3(-212.0682, -1324.342, 30.468223), -- Drawtext + Blip Coords
+        showBlip = true, -- If You Wany A Blip Or Not.
+        blipData = { -- https://docs.fivem.net/docs/game-references/blips/
+            sprite = 446, -- Blip Sprite
+            display = 4, -- Leave it 4
+            scale = 0.6, -- scale , i recommend 0.6
+            colour = 66, -- blip color - https://docs.fivem.net/docs/game-references/blips/ 
+            title = "Bennys", -- What Title you want for the blip or its just "Bennys"
             minZ = 29.468223, -- for polyzone(whre the drawtext will be)
             maxZ = 31.468223  -- for polyzone(whre the drawtext will be)
-    }
+        }
     },
+    --{ -- Mechanic 2
+        -- coords = vector3(-338.9632, -136.7556, 39.009613), -- Drawtext + Blip Coords
+        -- showBlip = true, -- If You Wany A Blip Or Not.
+        -- blipData = { -- https://docs.fivem.net/docs/game-references/blips/
+        --     sprite = 446, -- Blip Sprite
+        --     display = 4, -- Leave it 4
+        --     scale = 0.6, -- scale , i recommend 0.6
+        --     colour = 66, -- blip color - https://docs.fivem.net/docs/game-references/blips/ 
+        --     title = "Bennys", -- What Title you want for the blip or its just "Bennys"
+        --     minZ = 37.468223, -- for polyzone(whre the drawtext will be)
+        --     maxZ = 41.468223,  -- for polyzone(whre the drawtext will be)
+        --}
+    --},
 }
