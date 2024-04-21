93cb8c73b849bec320747797355be7450f0b17ed
royyalbert <101866296+royyalbert@users.noreply.github.com>
2024-04-21 20:35:12 +0300

    Update repairsystem-se.lua

--- cv-repairsystem/repairsystem-se.lua
+++ cv-repairsystem/repairsystem-se.lua
@@ -1,9 +1,9 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterNetEvent("cv-repair:repairButton",function(button,price)
+RegisterNetEvent("os-repair:repairButton",function(button,price)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if Player.Functions.RemoveMoney("cash", price) then
-        TriggerClientEvent("cv-repair:repair", src,button)
+        TriggerClientEvent("os-repair:repair", src,button)
     end
 end)
